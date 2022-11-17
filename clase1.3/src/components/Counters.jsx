2f12e0a77fe03651e5868a3e334cc73044d4292a
Cristhian Vargas Flores <cristhiansergiovargasflores@gmail.com>
2022-11-17 10:29:45 -0400

    se termino el ejercicio que dio el profe en clase de ocultar y mostrar el contador

--- clase1.3/src/components/Counters.jsx
+++ clase1.3/src/components/Counters.jsx
@@ -1,12 +1,20 @@
+import { useState } from 'react'
 import React from 'react'
 
 const Counters = () => {
 
-    const [first, setfirst] = useState(second)
+    const [count, setCount] = useState(0)
 
+    const plusOne = () => setCount(count + 1)
+    const reset = () => setCount(0)
+    const lessOne = () => setCount(count - 1)
+    
     return (
         <div>
-            <h2></h2>
+            <h2>{count}</h2>
+            <button onClick={plusOne}> +1 </button>
+            <button onClick={reset}> Reset </button>
+            <button onClick={lessOne}> -1 </button>
         </div>
     )
 }

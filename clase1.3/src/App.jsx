18cd55f50b4614f9e8f5174519eda36bc65063d1
crisLaptop <cristhiansergiovargasflores@gmail.com>
2022-11-16 23:06:55 -0400

    operadores condicionales y otros

---
+++ clase1.3/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+import Counters from './components/Counters'
+
+function App() {
+  const [hide, sethide] = useState(false)
+
+  function showHide() {
+    if (hide) {
+      sethide(false)
+    } else {
+      sethide(true)
+    }
+  }
+
+
+  return (
+    <div className="App">
+      <button onClick={showHide}> show/hide IMG </button>
+      {
+        hide ?
+          <Counters />
+          :
+          <h2>Estoy Oculto!!</h2>
+      }
+    </div>
+  )
+}
+
+export default App
---
+++ clase1.3/src/components/Counters.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Counters = () => {
+
+    const [first, setfirst] = useState(second)
+
+    return (
+        <div>
+            <h2></h2>
+        </div>
+    )
+}
+
+export default Counters

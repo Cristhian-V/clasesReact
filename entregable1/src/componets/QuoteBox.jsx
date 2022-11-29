1dabcc9a8463b96559f191626088928bb320afe6
Cristhian Vargas Flores <cristhiansergiovargasflores@gmail.com>
2022-11-29 15:00:46 -0400

    nose

---
+++ entregable1/src/componets/QuoteBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const QuoteBox = () => {
+  return (
+    <article>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro animi mollitia alias? Ratione saepe inventore ullam, dolor culpa quo quos?
+    </article>
+  )
+}
+
+export default QuoteBox
